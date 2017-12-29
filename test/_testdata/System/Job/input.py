9344d1bd3930b17aee57383b99b5ff5b4b25020a
lambdalisue <lambdalisue@hashnote.net>
2017-12-29 21:36:48 +0900

    Update

--- test/_testdata/System/Job/input.py
+++ test/_testdata/System/Job/input.py
@@ -1,8 +1,2 @@
-import sys
-
-if sys.version_info >= (3, 0):
-    name = input('Please input your name: ')
-else:
-    name = raw_input('Please input your name: ')
-
+name = input('Please input your name: ')
 print('Hello %s' % name)
